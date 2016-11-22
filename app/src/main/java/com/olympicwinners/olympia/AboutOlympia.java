297a9f374f3121049bca558fde66cab0c8f2de19
Zarowaaa <zarowaaa@gmail.com>
2016-11-22 12:47:45 +0100

    Working background music, functional menu

---
+++ app/src/main/java/com/olympicwinners/olympia/AboutOlympia.java
@@ -0,0 +1,15 @@
+package com.olympicwinners.olympia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutOlympia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_olympia);
+
+    }
+
+}
--- app/src/main/java/com/olympicwinners/olympia/MoodChoice.java
+++ app/src/main/java/com/olympicwinners/olympia/MoodChoice.java
@@ -1,12 +1,7 @@
 package com.olympicwinners.olympia;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.content.ComponentName;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,13 +9,11 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
-import static android.R.attr.value;
-
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class MoodMenu extends AppCompatActivity {
+public class MoodChoice extends AppCompatActivity {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -39,28 +32,12 @@ public class MoodMenu extends AppCompatActivity {
      */
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private boolean mIsBound = false;
-    private MusicService mServ;
-
     private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
         public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
     };
-    private View mControlsView;
     private final Runnable mShowPart2Runnable = new Runnable() {
         @Override
         public void run() {
@@ -69,7 +46,6 @@ public class MoodMenu extends AppCompatActivity {
             if (actionBar != null) {
                 actionBar.show();
             }
-            mControlsView.setVisibility(View.VISIBLE);
         }
     };
     private boolean mVisible;
@@ -98,80 +74,19 @@ public class MoodMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_mood_menu);
+        setContentView(R.layout.activity_mood_choice);
+
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+        //mControlsView = findViewById(R.id.fullscreen_content_controls);
+        //mContentView = findViewById(R.id.fullscreen_content);
 
 
-
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-        doBindService();
-
-        MusicService mServ = new MusicService();
-        Intent music = new Intent();
-        music.setClass(this,MusicService.class);
-        startService(music);
-
-        //
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.button4   ).setOnTouchListener(mDelayHideTouchListener);
-    }
-
-
-    private ServiceConnection Scon =new ServiceConnection(){
-
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder binder) {
-            mServ = ((MusicService.ServiceBinder) binder).getService();
-        }
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            mServ = null;
-        }
-    };
-
-    void doBindService(){
-        bindService(new Intent(this,MusicService.class),
-                Scon,MoodMenu.BIND_AUTO_CREATE);
-        mIsBound = true;
-    }
-
-    void doUnbindService()
-    {
-        if(mIsBound)
-        {
-            unbindService(Scon);
-            mIsBound = false;
-        }
-    }
-
-    public void openImage(View view){
-        Intent myIntent = new Intent(MoodMenu.this, SketchActivity.class);
-        MoodMenu.this.startActivity(myIntent);
-        mServ.resumeMusic();
-    }
-
-    public void openMoodMenu(View view){
-        Intent myIntent = new Intent(MoodMenu.this, MoodChoice.class);
-        MoodMenu.this.startActivity(myIntent);
-        doUnbindService();
-    }
-
-    public void openAbout(View view) {
-        Intent myIntent = new Intent(MoodMenu.this, AboutOlympia.class);
-        MoodMenu.this.startActivity(myIntent);
+       // findViewById(R.id.button4   ).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
@@ -198,7 +113,6 @@ public class MoodMenu extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
-        mControlsView.setVisibility(View.GONE);
         mVisible = false;
 
         // Schedule a runnable to remove the status and navigation bar after a delay
@@ -209,8 +123,6 @@ public class MoodMenu extends AppCompatActivity {
     @SuppressLint("InlinedApi")
     private void show() {
         // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         mVisible = true;
 
         // Schedule a runnable to display UI elements after a delay
@@ -225,5 +137,10 @@ public class MoodMenu extends AppCompatActivity {
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    public void startMenu(View view) {
+        Intent intent = new Intent(MoodChoice.this, MoodMenu.class);
+        MoodChoice.this.startActivity(intent);
     }
 }
